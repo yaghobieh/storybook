80eec6875c42c03b203dd4b49ee51f5acbac5277
John Yaghobieh <yagohbieh@gmail.com>
2024-10-01 12:50:59 +0300

    Add files via upload

---
+++ YourComponentName.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import { YourComponentNameProps } from "./YourComponentName.types";
+import './style.scss';
+
+export const YourComponentName: FC = (props: YourComponentNameProps) => {
+	return (
+		<div className="YourComponentName" />
+	);
+};
---
+++ index.ts
@@ -0,0 +1,2 @@
+export * from './YourComponentName';
+export type { YourComponentNameProps as IYourComponentName } from './YourComponentName.types';
